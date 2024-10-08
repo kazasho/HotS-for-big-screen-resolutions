be1b8b70b939237b6527da03fa7957b42d8ddd91
Kazasho <sbulling0@gmail.com>
2024-10-08 07:26:43 +0200

    Add debug logging

--- src/modules/File_permission_factory.py
+++ src/modules/File_permission_factory.py
@@ -10,13 +10,15 @@ class File_read_only(object):
 #   Function to remove read-only attribute
 
     def Remove_read_only(config_path, logger):
+        logger.debug("File permission factory: Trying to remove read-only attributte")
         os.chmod(config_path, S_IWUSR|S_IREAD)
-        logger.info("Permission factory: Removed read-only restriction")
+        logger.info("File permission factory: Removed read-only restriction")
 
 #   Function to add read-only attribute
 
     def Add_read_only(config_path, logger):
+        logger.debug("File permission factory: Trying to add read-only attributte")
         mode = os.stat(config_path).st_mode
         ro_mask = 0o777 ^ (stat.S_IWRITE | stat.S_IWGRP | stat.S_IWOTH)
         os.chmod(config_path, mode & ro_mask)
-        logger.info("Permission factory: Made file read-only")
+        logger.info("File permission factory: Made file read-only")
